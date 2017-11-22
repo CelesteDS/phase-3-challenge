0c8b57cb45efa282e94a6418ca19ca8a0f4b432a
Celeste De Santiago <celestedaphne@gmail.com>
2017-11-21 17:50:52 -0800

    Add working cart total, fix cart button positioning in part 3

--- part-3/public/main.js
+++ part-3/public/main.js
@@ -6,9 +6,18 @@ function addToCart() {
   const newCartItem = document.createElement('li');
   document.getElementById('cart-items').append(newCartItem);
   newCartItem.outerHTML = `<li class="cart-item flex flex-row-between"><span class="cart-item-name">${item}</span><span class="cart-item-price">$${price}</span></li>`;
+  const totalElement = document.getElementById('cart-total');
+  const newTotal = fixTotalNumber(Number(totalElement.innerHTML.match(/\$(.+)/)[1]) + price);
+  totalElement.innerHTML = `Total: $${newTotal}`;
+}
+
+function fixTotalNumber(total) {
+  const cents = Math.round(total * 100);
+  return cents / 100;
 }
 function clearCart() {
   document.getElementById('cart-item-count').innerHTML = '(0)';
+  document.getElementById('cart-total').innerHTML = 'Total: $0.00';
   document.getElementById('cart-items').outerHTML = '<ul class="item-section" id="cart-items"></ul>';
 }
 
@@ -25,9 +34,9 @@ buttons.forEach(function(button) {
   if (button.id === 'cart-button') {
     button.addEventListener('click', cartModal);
   } else if (button.id === 'clear-cart') {
-      button.addEventListener('click', clearCart);
+    button.addEventListener('click', clearCart);
   } else if (button.id === 'exit-cart') {
-      button.addEventListener('click', exitCart);
+    button.addEventListener('click', exitCart);
   } else {
     button.addEventListener('click', addToCart);
   }
